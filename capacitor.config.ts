060e1a174254eaa057b93af8bf62dd1c185971e2
Q-Control Mobile <qcontrol@mobile.com>
2026-09-01 21:03:34 +0100

    chore: add capacitor config and android platform

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.qcontrol.mobile',
+  appName: 'Q-Control',
+  webDir: 'public'
+};
+
+export default config;
